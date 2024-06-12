a64f00c04d6ef4e26f96f645b81054fa801bfddd
suraj <alphaguy433@gmail.com>
2024-06-12 23:28:38 +0530

    user login & signup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
